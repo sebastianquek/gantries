fefa5d31f1c82c711a9761dc62c26e4507a7d8ec
Sebastian Quek <sebq13@gmail.com>
2022-02-26 17:31:36 +0800

    refactor: test objects don't need multiple lines

--- scripts/__tests__/generate-layers.test.ts
+++ scripts/__tests__/generate-layers.test.ts
@@ -10,149 +10,65 @@ describe("generate-layers", () => {
     it("should throw error when there are rates with overlapping time ranges", () => {
       expect(() =>
         padGapsWithZeroRates([
-          {
-            StartTime: "00:00",
-            EndTime: "12:00",
-            ChargeAmount: 2,
-          },
-          {
-            StartTime: "10:00",
-            EndTime: "12:00",
-            ChargeAmount: 2,
-          },
+          { StartTime: "00:00", EndTime: "12:00", ChargeAmount: 2 },
+          { StartTime: "10:00", EndTime: "12:00", ChargeAmount: 2 },
         ])
       ).toThrowError("Overlap in intervals!");
     });
 
     it("should pad entire day with a $0 charge when no rates are provided", () => {
       expect(padGapsWithZeroRates([])).toStrictEqual([
-        {
-          StartTime: "00:00",
-          EndTime: "24:00",
-          ChargeAmount: 0,
-        },
+        { StartTime: "00:00", EndTime: "24:00", ChargeAmount: 0 },
       ]);
     });
 
     it("should pad correctly when there's 1 rate that spans the entire day", () => {
       expect(
         padGapsWithZeroRates([
-          {
-            StartTime: "00:00",
-            EndTime: "24:00",
-            ChargeAmount: 2,
-          },
+          { StartTime: "00:00", EndTime: "24:00", ChargeAmount: 2 },
         ])
       ).toStrictEqual([
-        {
-          StartTime: "00:00",
-          EndTime: "24:00",
-          ChargeAmount: 2,
-        },
+        { StartTime: "00:00", EndTime: "24:00", ChargeAmount: 2 },
       ]);
     });
 
     it("should pad correctly when there's only 1 rate", () => {
       expect(
         padGapsWithZeroRates([
-          {
-            StartTime: "08:00",
-            EndTime: "10:00",
-            ChargeAmount: 2,
-          },
+          { StartTime: "08:00", EndTime: "10:00", ChargeAmount: 2 },
         ])
       ).toStrictEqual([
-        {
-          StartTime: "00:00",
-          EndTime: "08:00",
-          ChargeAmount: 0,
-        },
-        {
-          StartTime: "08:00",
-          EndTime: "10:00",
-          ChargeAmount: 2,
-        },
-        {
-          StartTime: "10:00",
-          EndTime: "24:00",
-          ChargeAmount: 0,
-        },
+        { StartTime: "00:00", EndTime: "08:00", ChargeAmount: 0 },
+        { StartTime: "08:00", EndTime: "10:00", ChargeAmount: 2 },
+        { StartTime: "10:00", EndTime: "24:00", ChargeAmount: 0 },
       ]);
     });
 
     it("should pad correctly when there's 2 consecutive rates", () => {
       expect(
         padGapsWithZeroRates([
-          {
-            StartTime: "08:00",
-            EndTime: "10:00",
-            ChargeAmount: 2,
-          },
-          {
-            StartTime: "10:00",
-            EndTime: "11:00",
-            ChargeAmount: 3,
-          },
+          { StartTime: "08:00", EndTime: "10:00", ChargeAmount: 2 },
+          { StartTime: "10:00", EndTime: "11:00", ChargeAmount: 3 },
         ])
       ).toStrictEqual([
-        {
-          StartTime: "00:00",
-          EndTime: "08:00",
-          ChargeAmount: 0,
-        },
-        {
-          StartTime: "08:00",
-          EndTime: "10:00",
-          ChargeAmount: 2,
-        },
-        {
-          StartTime: "10:00",
-          EndTime: "11:00",
-          ChargeAmount: 3,
-        },
-        {
-          StartTime: "11:00",
-          EndTime: "24:00",
-          ChargeAmount: 0,
-        },
+        { StartTime: "00:00", EndTime: "08:00", ChargeAmount: 0 },
+        { StartTime: "08:00", EndTime: "10:00", ChargeAmount: 2 },
+        { StartTime: "10:00", EndTime: "11:00", ChargeAmount: 3 },
+        { StartTime: "11:00", EndTime: "24:00", ChargeAmount: 0 },
       ]);
     });
 
     it("should pad correctly when there's 2 rates with a gap between", () => {
       expect(
         padGapsWithZeroRates([
-          {
-            StartTime: "08:00",
-            EndTime: "10:00",
-            ChargeAmount: 2,
-          },
-          {
-            StartTime: "23:00",
-            EndTime: "24:00",
-            ChargeAmount: 3,
-          },
+          { StartTime: "08:00", EndTime: "10:00", ChargeAmount: 2 },
+          { StartTime: "23:00", EndTime: "24:00", ChargeAmount: 3 },
         ])
       ).toStrictEqual([
-        {
-          StartTime: "00:00",
-          EndTime: "08:00",
-          ChargeAmount: 0,
-        },
-        {
-          StartTime: "08:00",
-          EndTime: "10:00",
-          ChargeAmount: 2,
-        },
-        {
-          StartTime: "10:00",
-          EndTime: "23:00",
-          ChargeAmount: 0,
-        },
-        {
-          StartTime: "23:00",
-          EndTime: "24:00",
-          ChargeAmount: 3,
-        },
+        { StartTime: "00:00", EndTime: "08:00", ChargeAmount: 0 },
+        { StartTime: "08:00", EndTime: "10:00", ChargeAmount: 2 },
+        { StartTime: "10:00", EndTime: "23:00", ChargeAmount: 0 },
+        { StartTime: "23:00", EndTime: "24:00", ChargeAmount: 3 },
       ]);
     });
   });
@@ -165,160 +81,64 @@ describe("generate-layers", () => {
     it("should return the same rates if no transformations need to made", () => {
       expect(
         collapseRates([
-          {
-            StartTime: "00:00",
-            EndTime: "24:00",
-            ChargeAmount: 0,
-          },
+          { StartTime: "00:00", EndTime: "24:00", ChargeAmount: 0 },
         ])
       ).toStrictEqual([
-        {
-          StartTime: "00:00",
-          EndTime: "24:00",
-          IsOperational: false,
-        },
+        { StartTime: "00:00", EndTime: "24:00", IsOperational: false },
       ]);
     });
 
     it("should collapse positive rates together", () => {
       expect(
         collapseRates([
-          {
-            StartTime: "09:00",
-            EndTime: "10:00",
-            ChargeAmount: 1,
-          },
-          {
-            StartTime: "10:00",
-            EndTime: "11:00",
-            ChargeAmount: 2,
-          },
+          { StartTime: "09:00", EndTime: "10:00", ChargeAmount: 1 },
+          { StartTime: "10:00", EndTime: "11:00", ChargeAmount: 2 },
         ])
       ).toStrictEqual([
-        {
-          StartTime: "09:00",
-          EndTime: "11:00",
-          IsOperational: true,
-        },
+        { StartTime: "09:00", EndTime: "11:00", IsOperational: true },
       ]);
     });
 
     it("should collapse zero rates together", () => {
       expect(
         collapseRates([
-          {
-            StartTime: "09:00",
-            EndTime: "10:00",
-            ChargeAmount: 0,
-          },
-          {
-            StartTime: "10:00",
-            EndTime: "11:00",
-            ChargeAmount: 0,
-          },
+          { StartTime: "09:00", EndTime: "10:00", ChargeAmount: 0 },
+          { StartTime: "10:00", EndTime: "11:00", ChargeAmount: 0 },
         ])
       ).toStrictEqual([
-        {
-          StartTime: "09:00",
-          EndTime: "11:00",
-          IsOperational: false,
-        },
+        { StartTime: "09:00", EndTime: "11:00", IsOperational: false },
       ]);
     });
 
     it("should collapse positive rates together, and 0 rates together", () => {
       expect(
         collapseRates([
-          {
-            StartTime: "09:00",
-            EndTime: "10:00",
-            ChargeAmount: 1,
-          },
-          {
-            StartTime: "10:00",
-            EndTime: "11:00",
-            ChargeAmount: 2,
-          },
-          {
-            StartTime: "11:00",
-            EndTime: "12:00",
-            ChargeAmount: 0,
-          },
-          {
-            StartTime: "12:00",
-            EndTime: "13:00",
-            ChargeAmount: 0,
-          },
-          {
-            StartTime: "13:00",
-            EndTime: "14:00",
-            ChargeAmount: 2,
-          },
+          { StartTime: "09:00", EndTime: "10:00", ChargeAmount: 1 },
+          { StartTime: "10:00", EndTime: "11:00", ChargeAmount: 2 },
+          { StartTime: "11:00", EndTime: "12:00", ChargeAmount: 0 },
+          { StartTime: "12:00", EndTime: "13:00", ChargeAmount: 0 },
+          { StartTime: "13:00", EndTime: "14:00", ChargeAmount: 2 },
         ])
       ).toStrictEqual([
-        {
-          StartTime: "09:00",
-          EndTime: "11:00",
-          IsOperational: true,
-        },
-        {
-          StartTime: "11:00",
-          EndTime: "13:00",
-          IsOperational: false,
-        },
-        {
-          StartTime: "13:00",
-          EndTime: "14:00",
-          IsOperational: true,
-        },
+        { StartTime: "09:00", EndTime: "11:00", IsOperational: true },
+        { StartTime: "11:00", EndTime: "13:00", IsOperational: false },
+        { StartTime: "13:00", EndTime: "14:00", IsOperational: true },
       ]);
     });
 
     it("should collapse correctly when there are gaps in the time intervals", () => {
       expect(
         collapseRates([
-          {
-            StartTime: "09:00",
-            EndTime: "10:00",
-            ChargeAmount: 1,
-          },
-          {
-            StartTime: "12:00",
-            EndTime: "13:00",
-            ChargeAmount: 0,
-          },
-          {
-            StartTime: "14:00",
-            EndTime: "15:00",
-            ChargeAmount: 2,
-          },
-          {
-            StartTime: "16:00",
-            EndTime: "17:00",
-            ChargeAmount: 3,
-          },
+          { StartTime: "09:00", EndTime: "10:00", ChargeAmount: 1 },
+          { StartTime: "12:00", EndTime: "13:00", ChargeAmount: 0 },
+          { StartTime: "14:00", EndTime: "15:00", ChargeAmount: 2 },
+          { StartTime: "16:00", EndTime: "17:00", ChargeAmount: 3 },
         ])
       ).toStrictEqual([
-        {
-          StartTime: "09:00",
-          EndTime: "10:00",
-          IsOperational: true,
-        },
-        {
-          StartTime: "12:00",
-          EndTime: "13:00",
-          IsOperational: false,
-        },
-        {
-          StartTime: "14:00",
-          EndTime: "15:00",
-          IsOperational: true,
-        },
-        {
-          StartTime: "16:00",
-          EndTime: "17:00",
-          IsOperational: true,
-        },
+        { StartTime: "09:00", EndTime: "10:00", IsOperational: true },
+        { StartTime: "12:00", EndTime: "13:00", IsOperational: false },
+        { StartTime: "14:00", EndTime: "15:00", IsOperational: true },
+        { StartTime: "16:00", EndTime: "17:00", IsOperational: true },
       ]);
     });
   });
@@ -326,21 +146,8 @@ describe("generate-layers", () => {
   describe("splitRates", () => {
     it("should not split rates if splits is empty", () => {
       expect(
-        splitRates(
-          [
-            {
-              StartTime: "00:00",
-              EndTime: "10:00",
-            },
-          ],
-          []
-        )
-      ).toStrictEqual([
-        {
-          StartTime: "00:00",
-          EndTime: "10:00",
-        },
-      ]);
+        splitRates([{ StartTime: "00:00", EndTime: "10:00" }], [])
+      ).toStrictEqual([{ StartTime: "00:00", EndTime: "10:00" }]);
     });
 
     it("should return empty array if rates are empty", () => {
@@ -350,62 +157,31 @@ describe("generate-layers", () => {
 
     it("should split a single rate into 2", () => {
       expect(
-        splitRates(
-          [
-            {
-              StartTime: "00:00",
-              EndTime: "10:00",
-            },
-          ],
-          ["08:00"]
-        )
+        splitRates([{ StartTime: "00:00", EndTime: "10:00" }], ["08:00"])
       ).toStrictEqual([
-        {
-          StartTime: "00:00",
-          EndTime: "08:00",
-        },
-        {
-          StartTime: "08:00",
-          EndTime: "10:00",
-        },
+        { StartTime: "00:00", EndTime: "08:00" },
+        { StartTime: "08:00", EndTime: "10:00" },
       ]);
     });
 
     it("should not split rates whose start time is same as a split", () => {
       expect(
-        splitRates(
-          [
-            {
-              StartTime: "00:00",
-              EndTime: "10:00",
-            },
-          ],
-          ["00:00"]
-        )
-      ).toStrictEqual([
-        {
-          StartTime: "00:00",
-          EndTime: "10:00",
-        },
-      ]);
+        splitRates([{ StartTime: "00:00", EndTime: "10:00" }], ["00:00"])
+      ).toStrictEqual([{ StartTime: "00:00", EndTime: "10:00" }]);
     });
 
     it("should not split rates whose end time is same as a split", () => {
       expect(
         splitRates(
           [
-            {
-              StartTime: "00:00",
-              EndTime: "10:00",
-            },
+            { StartTime: "07:00", EndTime: "08:00" },
+            { StartTime: "08:00", EndTime: "10:00" },
           ],
           ["10:00"]
         )
       ).toStrictEqual([
-        {
-          StartTime: "00:00",
-          EndTime: "10:00",
-        },
+        { StartTime: "07:00", EndTime: "08:00" },
+        { StartTime: "08:00", EndTime: "10:00" },
       ]);
     });
 
@@ -413,57 +189,28 @@ describe("generate-layers", () => {
       expect(
         splitRates(
           [
-            {
-              StartTime: "09:00",
-              EndTime: "10:00",
-            },
-            {
-              StartTime: "12:00",
-              EndTime: "13:00",
-            },
+            { StartTime: "09:00", EndTime: "10:00" },
+            { StartTime: "12:00", EndTime: "13:00" },
           ],
           ["08:00", "11:00"]
         )
       ).toStrictEqual([
-        {
-          StartTime: "09:00",
-          EndTime: "10:00",
-        },
-        {
-          StartTime: "12:00",
-          EndTime: "13:00",
-        },
+        { StartTime: "09:00", EndTime: "10:00" },
+        { StartTime: "12:00", EndTime: "13:00" },
       ]);
     });
 
     it("should split a single rate multiple times", () => {
       expect(
         splitRates(
-          [
-            {
-              StartTime: "08:00",
-              EndTime: "12:00",
-            },
-          ],
+          [{ StartTime: "08:00", EndTime: "12:00" }],
           ["07:00", "09:00", "10:00", "11:00"]
         )
       ).toStrictEqual([
-        {
-          StartTime: "08:00",
-          EndTime: "09:00",
-        },
-        {
-          StartTime: "09:00",
-          EndTime: "10:00",
-        },
-        {
-          StartTime: "10:00",
-          EndTime: "11:00",
-        },
-        {
-          StartTime: "11:00",
-          EndTime: "12:00",
-        },
+        { StartTime: "08:00", EndTime: "09:00" },
+        { StartTime: "09:00", EndTime: "10:00" },
+        { StartTime: "10:00", EndTime: "11:00" },
+        { StartTime: "11:00", EndTime: "12:00" },
       ]);
     });
 
@@ -471,50 +218,18 @@ describe("generate-layers", () => {
       expect(
         splitRates(
           [
-            {
-              StartTime: "08:00",
-              EndTime: "12:00",
-              AdditionalProperty: "hello",
-            },
-            {
-              StartTime: "12:00",
-              EndTime: "14:00",
-              ChargeAmount: 2,
-            },
-            {
-              StartTime: "14:00",
-              EndTime: "15:00",
-              IsOperational: true,
-            },
+            { StartTime: "08:00", EndTime: "12:00", NewProperty: "hello" },
+            { StartTime: "12:00", EndTime: "14:00", ChargeAmount: 2 },
+            { StartTime: "14:00", EndTime: "15:00", IsOperational: true },
           ],
           ["07:00", "09:00", "13:00"]
         )
       ).toStrictEqual([
-        {
-          StartTime: "08:00",
-          EndTime: "09:00",
-          AdditionalProperty: "hello",
-        },
-        {
-          StartTime: "09:00",
-          EndTime: "12:00",
-          AdditionalProperty: "hello",
-        },
-        {
-          StartTime: "12:00",
-          EndTime: "13:00",
-          ChargeAmount: 2,
-        },
-        {
-          StartTime: "13:00",
-          EndTime: "14:00",
-          ChargeAmount: 2,
-        },
-        {
-          StartTime: "14:00",
-          EndTime: "15:00",
-          IsOperational: true,
-        },
+        { StartTime: "08:00", EndTime: "09:00", NewProperty: "hello" },
+        { StartTime: "09:00", EndTime: "12:00", NewProperty: "hello" },
+        { StartTime: "12:00", EndTime: "13:00", ChargeAmount: 2 },
+        { StartTime: "13:00", EndTime: "14:00", ChargeAmount: 2 },
+        { StartTime: "14:00", EndTime: "15:00", IsOperational: true },
       ]);
     });
   });
