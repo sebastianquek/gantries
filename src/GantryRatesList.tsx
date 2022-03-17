7529c7781a12617133f42fc37688cee5c0f67cdf
Sebastian Quek <sebq13@gmail.com>
2022-03-17 23:13:50 +0800

    fix: issue where filtered rates kept expanding

--- src/GantryRatesList.tsx
+++ src/GantryRatesList.tsx
@@ -63,9 +63,11 @@ export const GantryRatesList = ({
     const idx = filteredRates.findIndex(({ startTime, endTime }) => {
       return time >= startTime && time < endTime;
     });
+    // Ensure at least 0 so the slice doesn't take incorrect elements
+    const loopAroundIdx = Math.max(0, 4 - (rates.length - idx));
     filteredRates = [
-      ...rates.slice(idx),
-      ...rates.slice(0, 4 - (rates.length - idx)),
+      ...rates.slice(idx, idx + 4),
+      ...rates.slice(0, loopAroundIdx),
     ];
   }
   return (
