39e5c5637f025cf85411471a9e82a5d0fcb7c0d7
Sebastian Quek <sebq13@gmail.com>
2022-06-19 16:34:41 +0800

    fix: resolve issue where gantry couldn't be found via queryMap

--- src/Map.tsx
+++ src/Map.tsx
@@ -51,7 +51,11 @@ export const Map = () => {
     mapRef: mapRef,
     initialLng: 103.837,
     initialLat: 1.31,
-    initialZoom: 12.15,
+    // Ensures that if a gantry param is provided, it can be found via queryMap.
+    // Having a high initial zoom may not render gantries outside of the
+    // viewport (mapbox optimisation) which causes queryMap to return null incorrectly.
+    // Setting to 11 ensures the gantries at the boundary can still be queried.
+    initialZoom: gantryId ? 11 : 12.15,
     mapStyle: "STREETS",
   });
 
