b19c2e1e588f433018ca8c46842961e510442195
Sebastian Quek <sebq13@gmail.com>
2022-06-26 11:26:10 +0800

    test: add simple map test

---
+++ src/tests/map.e2e.test.ts
@@ -0,0 +1,18 @@
+import { test, expect } from "@playwright/test";
+
+test.beforeEach(async ({ page }) => {
+  await page.goto("/");
+});
+
+test.describe("when filters are set to a timing without operational gantries", () => {
+  test.beforeEach(async ({ page }) => {
+    await page
+      .locator('select[data-test-id="day-type"]')
+      .selectOption("Weekdays");
+    await page.locator('[data-test-id="time-filter"]').fill("00:00");
+  });
+
+  test("should see no gantries highlighted", async ({ page }) => {
+    await expect(page.locator('[aria-label="Map"]')).toHaveScreenshot();
+  });
+});
