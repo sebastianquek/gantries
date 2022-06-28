b74526785e2c14886cec468eda0faadd618bcace
Sebastian Quek <sebq13@gmail.com>
2022-06-28 11:08:59 +0800

    ci: run webkit, mobile-safari on macos while others run on ubuntu

--- playwright.config.ts
+++ playwright.config.ts
@@ -53,13 +53,13 @@ const config: PlaywrightTestConfig = {
       testIgnore: /.*.mobile.e2e.test.ts/,
     },
 
-    // {
-    //   name: "firefox",
-    //   use: {
-    //     ...devices["Desktop Firefox"],
-    //   },
-    //   testIgnore: /.*.mobile.e2e.test.ts/,
-    // },
+    {
+      name: "firefox",
+      use: {
+        ...devices["Desktop Firefox"],
+      },
+      testIgnore: /.*.mobile.e2e.test.ts/,
+    },
 
     {
       name: "webkit",
