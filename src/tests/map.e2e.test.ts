731bc15db0a0b591e123cdc5851034d135006fe7
Sebastian Quek <sebq13@gmail.com>
2022-06-29 19:24:58 +0800

    test: add map test to verify that some gantries show the amount label [snapshot]

--- src/tests/map.e2e.test.ts
+++ src/tests/map.e2e.test.ts
@@ -32,3 +32,26 @@ test.describe("when filters are set to a timing without operational gantries", (
     await expect(locator).toHaveScreenshot();
   });
 });
+
+test.describe("when filters are set to a timing with operational gantries", () => {
+  test.beforeEach(async ({ page }) => {
+    await page
+      .locator('select[data-test-id="day-type"]')
+      .selectOption("Weekdays");
+    await page.locator('[data-test-id="time-filter"]').fill("08:32");
+  });
+
+  test("[snapshot] should see some gantries with amount labels", async ({
+    page,
+  }) => {
+    const locator = page.locator('[aria-label="Map"]');
+
+    // Move the map to the front so the screenshot only takes the map without other elements (e.g. alert banner)
+    // TODO: move this out to a custom matcher once Playwright is able to
+    // have custom matchers that can reference other in-built matchers (i.e. toHaveScreenshot)
+    const elementHandle = await locator.elementHandle();
+    await elementHandle?.evaluate((node) => (node.style.zIndex = "9999"));
+
+    await expect(locator).toHaveScreenshot();
+  });
+});
