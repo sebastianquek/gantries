0134964c169774da73450f3b4bab2e43189146a7
Sebastian Quek <sebq13@gmail.com>
2022-07-01 14:13:56 +0800

    refactor: test page object model for map [snapshot]

--- src/tests/map.desktop.e2e.test.ts
+++ src/tests/map.desktop.e2e.test.ts
@@ -1,297 +1,74 @@
-import { test, expect } from "@playwright/test";
+import { test } from "@playwright/test";
 
-test.beforeEach(async ({ page }) => {
-  await Promise.all([
-    page.waitForFunction(async () => {
-      const isIdle = await new Promise<boolean>((res) => {
-        document.addEventListener("idle", () => res(true), { once: true });
+import { Map } from "./models/map";
+
+for (const noGantriesOn of [false, true]) {
+  let map: Map;
+
+  test.beforeEach(async ({ page }) => {
+    map = new Map(page, "DESKTOP");
+    await map.goto();
+  });
+
+  test.describe(`when filters are set to a timing ${
+    noGantriesOn ? "without" : "with"
+  } operational gantries`, () => {
+    test.beforeEach(async () => {
+      if (noGantriesOn) {
+        await map.initNoGantriesOn();
+      } else {
+        await map.initSomeGantriesOn();
+      }
+    });
+
+    test("[snapshot] should highlight and center the gantry when clicked", async () => {
+      await map.mapCanvas.click({
+        // click on gantry 34
+        position: {
+          x: 734,
+          y: 281,
+        },
       });
-      return isIdle;
-    }),
-    page.goto("/"),
-  ]);
-});
-
-test.describe("when filters are set to a timing without operational gantries", () => {
-  test.beforeEach(async ({ page }) => {
-    await page
-      .locator('select[data-test-id="day-type"]')
-      .selectOption("Weekdays");
-    await page.locator('[data-test-id="time-filter"]').fill("00:00");
-  });
-
-  test("[snapshot] should highlight and center the gantry when clicked", async ({
-    page,
-  }) => {
-    const locator = page.locator('[aria-label="Map"]');
-    await locator.click({
-      position: {
-        x: 734,
-        y: 281,
-      },
+      await map.shouldMatchMapSnapshot();
     });
 
-    await expect(page.locator("text=CTE from Balestier Road")).toBeVisible();
-
-    // Move the map to the front so the screenshot only takes the map without other elements (e.g. alert banner)
-    // TODO: move this out to a custom matcher once Playwright is able to
-    // have custom matchers that can reference other in-built matchers (i.e. toHaveScreenshot)
-    const elementHandle = await locator.elementHandle();
-    await elementHandle?.evaluate((node) => (node.style.zIndex = "9999"));
-
-    await expect(locator).toHaveScreenshot();
-  });
-
-  test("[snapshot] should move the map on drag", async ({ page }) => {
-    await page.mouse.move(720, 170);
-    await page.mouse.down();
-    await page.mouse.move(470, 500);
-    await page.mouse.up();
-
-    const locator = page.locator('[aria-label="Map"]');
-
-    // Move the map to the front so the screenshot only takes the map without other elements (e.g. alert banner)
-    // TODO: move this out to a custom matcher once Playwright is able to
-    // have custom matchers that can reference other in-built matchers (i.e. toHaveScreenshot)
-    const elementHandle = await locator.elementHandle();
-    await elementHandle?.evaluate((node) => (node.style.zIndex = "9999"));
-
-    await expect(locator).toHaveScreenshot();
-  });
-
-  test("[snapshot] should zoom the map on scroll", async ({ page }) => {
-    await page.mouse.move(720, 170);
-    await page.mouse.wheel(0, -5000);
-
-    // Wait for map to zoom in as the wheel method does not wait for the scrolling to finish before returning
-    // https://playwright.dev/docs/api/class-mouse#mouse-wheel
-    await page.waitForTimeout(2000);
-
-    const locator = page.locator('[aria-label="Map"]');
-
-    // Move the map to the front so the screenshot only takes the map without other elements (e.g. alert banner)
-    // TODO: move this out to a custom matcher once Playwright is able to
-    // have custom matchers that can reference other in-built matchers (i.e. toHaveScreenshot)
-    const elementHandle = await locator.elementHandle();
-    await elementHandle?.evaluate((node) => (node.style.zIndex = "9999"));
-
-    await expect(locator).toHaveScreenshot();
-  });
-
-  test("[snapshot] should zoom in the map after clicking on a gantry if the map is zoomed out too far", async ({
-    page,
-  }) => {
-    const locator = page.locator('[aria-label="Map"]');
-
-    await page.mouse.move(720, 170);
-    await page.mouse.wheel(0, 5000);
-
-    // Wait for map to zoom in as the wheel method does not wait for the scrolling to finish before returning
-    // https://playwright.dev/docs/api/class-mouse#mouse-wheel
-    await page.waitForTimeout(2000);
-
-    const elementHandle = await locator.elementHandle();
-    await elementHandle?.evaluate((node) => (node.style.zIndex = "9999"));
-
-    // Should be zoomed out
-    await expect(locator).toHaveScreenshot();
-
-    await elementHandle?.evaluate((node) => (node.style.zIndex = "unset"));
-
-    await locator.click({
-      position: {
-        x: 734,
-        y: 281,
-      },
+    test("[snapshot] should move the map on drag", async () => {
+      await map.dragMap();
+      await map.shouldMatchMapSnapshot();
     });
 
-    // Move the map to the front so the screenshot only takes the map without other elements (e.g. alert banner)
-    // TODO: move this out to a custom matcher once Playwright is able to
-    // have custom matchers that can reference other in-built matchers (i.e. toHaveScreenshot)
-    await elementHandle?.evaluate((node) => (node.style.zIndex = "9999"));
-
-    // Should be zoomed back in
-    await expect(locator).toHaveScreenshot();
-  });
-
-  test("[snapshot] should maintain zoom after clicking on a gantry if the map is already zoomed in sufficiently", async ({
-    page,
-  }) => {
-    const locator = page.locator('[aria-label="Map"]');
-
-    await page.mouse.move(720, 170);
-    await page.mouse.wheel(0, -5000);
-
-    // Wait for map to zoom in as the wheel method does not wait for the scrolling to finish before returning
-    // https://playwright.dev/docs/api/class-mouse#mouse-wheel
-    await page.waitForTimeout(2000);
-
-    await page.mouse.wheel(0, -5000);
-    await page.waitForTimeout(2000);
-
-    const elementHandle = await locator.elementHandle();
-    await elementHandle?.evaluate((node) => (node.style.zIndex = "9999"));
-
-    // Should be zoomed in
-    await expect(locator).toHaveScreenshot();
-
-    await elementHandle?.evaluate((node) => (node.style.zIndex = "unset"));
-
-    await locator.click({
-      position: {
-        x: 1002,
-        y: 545,
-      },
+    test("[snapshot] should zoom the map on scroll", async () => {
+      await map.zoomInMap();
+      await map.shouldMatchMapSnapshot();
     });
 
-    // Move the map to the front so the screenshot only takes the map without other elements (e.g. alert banner)
-    // TODO: move this out to a custom matcher once Playwright is able to
-    // have custom matchers that can reference other in-built matchers (i.e. toHaveScreenshot)
-    await elementHandle?.evaluate((node) => (node.style.zIndex = "9999"));
+    test("[snapshot] should zoom in the map after clicking on a gantry if the map is zoomed out too far", async () => {
+      await map.zoomOutMap();
+      await map.shouldMatchMapSnapshot();
 
-    // Should maintain zoom
-    await expect(locator).toHaveScreenshot();
-  });
-});
-
-test.describe("when filters are set to a timing with operational gantries", () => {
-  test.beforeEach(async ({ page }) => {
-    await page
-      .locator('select[data-test-id="day-type"]')
-      .selectOption("Weekdays");
-    await page.locator('[data-test-id="time-filter"]').fill("08:32");
-  });
-
-  test("[snapshot] should highlight and center the gantry when clicked", async ({
-    page,
-  }) => {
-    const locator = page.locator('[aria-label="Map"]');
-    await locator.click({
-      position: {
-        x: 734,
-        y: 281,
-      },
+      // click on gantry 34
+      await map.mapCanvas.click({
+        position: {
+          x: 734,
+          y: 281,
+        },
+      });
+      await map.shouldMatchMapSnapshot();
     });
 
-    await expect(page.locator("text=CTE from Balestier Road")).toBeVisible();
+    test("[snapshot] should maintain zoom after clicking on a gantry if the map is already zoomed in sufficiently", async () => {
+      await map.zoomInMap();
+      await map.zoomInMap();
+      await map.shouldMatchMapSnapshot();
 
-    // Move the map to the front so the screenshot only takes the map without other elements (e.g. alert banner)
-    // TODO: move this out to a custom matcher once Playwright is able to
-    // have custom matchers that can reference other in-built matchers (i.e. toHaveScreenshot)
-    const elementHandle = await locator.elementHandle();
-    await elementHandle?.evaluate((node) => (node.style.zIndex = "9999"));
-
-    await expect(locator).toHaveScreenshot();
-  });
-
-  test("[snapshot] should move the map on drag", async ({ page }) => {
-    await page.mouse.move(720, 170);
-    await page.mouse.down();
-    await page.mouse.move(470, 500);
-    await page.mouse.up();
-
-    const locator = page.locator('[aria-label="Map"]');
-
-    // Move the map to the front so the screenshot only takes the map without other elements (e.g. alert banner)
-    // TODO: move this out to a custom matcher once Playwright is able to
-    // have custom matchers that can reference other in-built matchers (i.e. toHaveScreenshot)
-    const elementHandle = await locator.elementHandle();
-    await elementHandle?.evaluate((node) => (node.style.zIndex = "9999"));
-
-    await expect(locator).toHaveScreenshot();
-  });
-
-  test("[snapshot] should zoom the map on scroll", async ({ page }) => {
-    await page.mouse.move(720, 170);
-    await page.mouse.wheel(0, -5000);
-
-    // Wait for map to zoom in as the wheel method does not wait for the scrolling to finish before returning
-    // https://playwright.dev/docs/api/class-mouse#mouse-wheel
-    await page.waitForTimeout(2000);
-
-    const locator = page.locator('[aria-label="Map"]');
-
-    // Move the map to the front so the screenshot only takes the map without other elements (e.g. alert banner)
-    // TODO: move this out to a custom matcher once Playwright is able to
-    // have custom matchers that can reference other in-built matchers (i.e. toHaveScreenshot)
-    const elementHandle = await locator.elementHandle();
-    await elementHandle?.evaluate((node) => (node.style.zIndex = "9999"));
-
-    await expect(locator).toHaveScreenshot();
-  });
-
-  test("[snapshot] should zoom in the map after clicking on a gantry if the map is zoomed out too far", async ({
-    page,
-  }) => {
-    const locator = page.locator('[aria-label="Map"]');
-
-    await page.mouse.move(720, 170);
-    await page.mouse.wheel(0, 5000);
-
-    // Wait for map to zoom in as the wheel method does not wait for the scrolling to finish before returning
-    // https://playwright.dev/docs/api/class-mouse#mouse-wheel
-    await page.waitForTimeout(2000);
-
-    const elementHandle = await locator.elementHandle();
-    await elementHandle?.evaluate((node) => (node.style.zIndex = "9999"));
-
-    // Should be zoomed out
-    await expect(locator).toHaveScreenshot();
-
-    await elementHandle?.evaluate((node) => (node.style.zIndex = "unset"));
-
-    await locator.click({
-      position: {
-        x: 734,
-        y: 281,
-      },
+      // click on gantry 34
+      await map.mapCanvas.click({
+        position: {
+          x: 1024,
+          y: 294,
+        },
+      });
+      await map.shouldMatchMapSnapshot();
     });
-
-    // Move the map to the front so the screenshot only takes the map without other elements (e.g. alert banner)
-    // TODO: move this out to a custom matcher once Playwright is able to
-    // have custom matchers that can reference other in-built matchers (i.e. toHaveScreenshot)
-    await elementHandle?.evaluate((node) => (node.style.zIndex = "9999"));
-
-    // Should be zoomed back in
-    await expect(locator).toHaveScreenshot();
   });
-
-  test("[snapshot] should maintain zoom after clicking on a gantry if the map is already zoomed in sufficiently", async ({
-    page,
-  }) => {
-    const locator = page.locator('[aria-label="Map"]');
-
-    await page.mouse.move(720, 170);
-    await page.mouse.wheel(0, -5000);
-
-    // Wait for map to zoom in as the wheel method does not wait for the scrolling to finish before returning
-    // https://playwright.dev/docs/api/class-mouse#mouse-wheel
-    await page.waitForTimeout(2000);
-
-    await page.mouse.wheel(0, -5000);
-    await page.waitForTimeout(2000);
-
-    const elementHandle = await locator.elementHandle();
-    await elementHandle?.evaluate((node) => (node.style.zIndex = "9999"));
-
-    // Should be zoomed in
-    await expect(locator).toHaveScreenshot();
-
-    await elementHandle?.evaluate((node) => (node.style.zIndex = "unset"));
-
-    await locator.click({
-      position: {
-        x: 1002,
-        y: 545,
-      },
-    });
-
-    // Move the map to the front so the screenshot only takes the map without other elements (e.g. alert banner)
-    // TODO: move this out to a custom matcher once Playwright is able to
-    // have custom matchers that can reference other in-built matchers (i.e. toHaveScreenshot)
-    await elementHandle?.evaluate((node) => (node.style.zIndex = "9999"));
-
-    // Should maintain zoom
-    await expect(locator).toHaveScreenshot();
-  });
-});
+}
--- src/tests/map.e2e.test.ts
+++ src/tests/map.e2e.test.ts
@@ -1,117 +1,57 @@
-import { test, expect } from "@playwright/test";
+import { test } from "@playwright/test";
 
-test.describe("when filters are set to a timing without operational gantries", () => {
+import { Map } from "./models/map";
+
+for (const noGantriesOn of [false, true]) {
+  let map: Map;
+
   test.beforeEach(async ({ page }) => {
-    await Promise.all([
-      page.waitForFunction(async () => {
-        const isIdle = await new Promise<boolean>((res) => {
-          document.addEventListener("idle", () => res(true), { once: true });
-        });
-        return isIdle;
-      }),
-      page.goto("/"),
-    ]);
-    await page
-      .locator('select[data-test-id="day-type"]')
-      .selectOption("Weekdays");
-    await page.locator('[data-test-id="time-filter"]').fill("00:00");
+    map = new Map(page, "DESKTOP"); // Screen-type doesn't matter
+    await map.goto();
   });
 
-  test("[snapshot] should see no gantries with amount labels", async ({
-    page,
-  }) => {
-    const locator = page.locator('[aria-label="Map"]');
+  test.describe(`when filters are set to a timing ${
+    noGantriesOn ? "without" : "with"
+  } operational gantries`, () => {
+    test.beforeEach(async () => {
+      if (noGantriesOn) {
+        await map.initNoGantriesOn();
+      } else {
+        await map.initSomeGantriesOn();
+      }
+    });
 
-    // Move the map to the front so the screenshot only takes the map without other elements (e.g. alert banner)
-    // TODO: move this out to a custom matcher once Playwright is able to
-    // have custom matchers that can reference other in-built matchers (i.e. toHaveScreenshot)
-    const elementHandle = await locator.elementHandle();
-    await elementHandle?.evaluate((node) => (node.style.zIndex = "9999"));
-
-    await expect(locator).toHaveScreenshot();
+    test(`[snapshot] should see ${
+      noGantriesOn ? "no" : "some"
+    } gantries with amount labels`, async () => {
+      await map.shouldMatchMapSnapshot();
+    });
   });
-});
-
-test.describe("when filters are set to a timing with operational gantries", () => {
-  test.beforeEach(async ({ page }) => {
-    await Promise.all([
-      page.waitForFunction(async () => {
-        const isIdle = await new Promise<boolean>((res) => {
-          document.addEventListener("idle", () => res(true), { once: true });
-        });
-        return isIdle;
-      }),
-      page.goto("/"),
-    ]);
-    await page
-      .locator('select[data-test-id="day-type"]')
-      .selectOption("Weekdays");
-    await page.locator('[data-test-id="time-filter"]').fill("08:32");
-  });
-
-  test("[snapshot] should see some gantries with amount labels", async ({
-    page,
-  }) => {
-    const locator = page.locator('[aria-label="Map"]');
-
-    // Move the map to the front so the screenshot only takes the map without other elements (e.g. alert banner)
-    // TODO: move this out to a custom matcher once Playwright is able to
-    // have custom matchers that can reference other in-built matchers (i.e. toHaveScreenshot)
-    const elementHandle = await locator.elementHandle();
-    await elementHandle?.evaluate((node) => (node.style.zIndex = "9999"));
-
-    await expect(locator).toHaveScreenshot();
-  });
-});
+}
 
 test.describe("when navigating to a gantry URL", () => {
-  test.beforeEach(async ({ page }) => {
-    await Promise.all([
-      page.waitForFunction(async () => {
-        const isIdle = await new Promise<boolean>((res) => {
-          document.addEventListener("idle", () => res(true), { once: true });
-        });
-        return isIdle;
-      }),
-      page.goto("/42"),
-    ]);
-  });
+  for (const noGantriesOn of [false, true]) {
+    let map: Map;
 
-  test("[snapshot] should center the map to the gantry when filters are set to a timing without operational gantries", async ({
-    page,
-  }) => {
-    await page
-      .locator('select[data-test-id="day-type"]')
-      .selectOption("Weekdays");
-    await page.locator('[data-test-id="time-filter"]').fill("00:00");
+    test.beforeEach(async ({ page }) => {
+      map = new Map(page, "DESKTOP"); // Screen-type doesn't matter
+      await map.goto({ gantryId: true });
+    });
 
-    const locator = page.locator('[aria-label="Map"]');
+    test.describe(`when filters are set to a timing ${
+      noGantriesOn ? "without" : "with"
+    } operational gantries`, () => {
+      test.beforeEach(async () => {
+        if (noGantriesOn) {
+          await map.initNoGantriesOn();
+        } else {
+          await map.initSomeGantriesOn();
+        }
+      });
 
-    // Move the map to the front so the screenshot only takes the map without other elements (e.g. alert banner)
-    // TODO: move this out to a custom matcher once Playwright is able to
-    // have custom matchers that can reference other in-built matchers (i.e. toHaveScreenshot)
-    const elementHandle = await locator.elementHandle();
-    await elementHandle?.evaluate((node) => (node.style.zIndex = "9999"));
-
-    await expect(locator).toHaveScreenshot();
-  });
-
-  test("[snapshot] should center the map to the gantry when filters are set to a timing with operational gantries", async ({
-    page,
-  }) => {
-    await page
-      .locator('select[data-test-id="day-type"]')
-      .selectOption("Weekdays");
-    await page.locator('[data-test-id="time-filter"]').fill("08:32");
-
-    const locator = page.locator('[aria-label="Map"]');
-
-    // Move the map to the front so the screenshot only takes the map without other elements (e.g. alert banner)
-    // TODO: move this out to a custom matcher once Playwright is able to
-    // have custom matchers that can reference other in-built matchers (i.e. toHaveScreenshot)
-    const elementHandle = await locator.elementHandle();
-    await elementHandle?.evaluate((node) => (node.style.zIndex = "9999"));
-
-    await expect(locator).toHaveScreenshot();
-  });
+      test(`[snapshot] should center the map to the gantry`, async () => {
+        await map.shouldMatchMapSnapshot();
+      });
+    });
+  }
 });
